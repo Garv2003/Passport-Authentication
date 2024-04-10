bbc6063e602918af9f18f47c3b0dd035d5e4c430
Garv2003 <aggarwalgarv0505@gmail.com>
2024-04-11 00:29:59 +0530

    resolve

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/public")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
