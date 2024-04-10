240f6cf98755b73fc8028f92acfd2a6a6dcf7f50
Garv2003 <aggarwalgarv0505@gmail.com>
2024-04-11 00:29:31 +0530

    resolve

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(passport.initialize());
